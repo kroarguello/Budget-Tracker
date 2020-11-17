bf9a461d06ebd2a6efb6ecbefc5c1b60c45920a3
Delmy Arguello <kroarguelloadel@gmail.com>
2020-11-17 10:36:14 -0600

    updating

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,13 +3,13 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
     "/",
-    "/index.html",
-    "/index.js",
-    "/db.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-    "/styles.css",
-    "/manifest.webmanifest"
+    "/db.js",
+    "/index.html",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css"
 ];
 
 self.addEventListener("install", function (evt) {
@@ -46,25 +46,22 @@ self.addEventListener("fetch", function (evt) {
     if (evt.request.url.includes("/api/")) {
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(evt.request)
-                    .then(response => {
-                        // If the response was good, clone it and store it in the cache.
-                        if (response.status === 200) {
-                            cache.put(evt.request.url, response.clone());
-                        }
-
-                        return response;
-                    })
-                    .catch(err => {
-                        // Network request failed, try to get it from the cache.
-                        return cache.match(evt.request);
-                    });
+            return fetch(evt.request)
+            .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+                }
+            return response;
+              })
+             .catch(err => {
+             // Network request failed, try to get it from the cache.
+             return cache.match(evt.request);
+             });
             }).catch(err => console.log(err))
         );
-
         return;
     }
-
     // if the request is not for the API, serve static assets using "offline-first" approach.
     // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
     evt.respondWith(
