2b382a4491dc1891455bb45248de115d3332d2c3
Delmy Arguello <kroarguelloadel@gmail.com>
2020-11-17 16:10:28 -0600

    deploying

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const logger = require("morgan");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -26,7 +26,7 @@ mongoose.connect(
 );
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
